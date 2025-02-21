eb671ffc6f4004ea1c56387b64789266449e8913
AbhiChougule <abhijeetchougule188@gmail.com>
2025-02-21 09:46:35 +0530

    made changes in code of main.py file

--- main.py
+++ main.py
@@ -1,49 +1,44 @@
 def email_extraction(data):
-    words=[]
+    emails = []
     for line in data:
         for word in line.split(','):
-            if '@' in word:
-                if word.endswith('.com'):
-                    words.append(word)
-    return words
+            if '@' in word and word.endswith('.com'):
+                emails.append(word)
+    return emails
 
 def number_extraction(data):
-    no=[]
+    numbers = []
     for line in data:
         for word in line.split(','):
             if word.isdigit() and len(word) in [9, 10]:
-                no.append(word)
-    return no
+                numbers.append(word)
+    return numbers
 
 def special_char(data):
-    char=[]
+    special_characters = []
     for line in data:
         for word in line.split(','):
             if not word.isalpha() and word.isdigit():
-                char.append(word)
-    return word
-
-
+                special_characters.append(word)
+    return special_characters
 
 
 def main():
-    filename="test1.txt"
-    fp = open(filename, "r")
-    data = fp.readlines()
-    emails=email_extraction(data)
+    filename = 'test1.txt'
+    with open(filename, 'r') as fp:
+        data = fp.readlines()
+    emails = email_extraction(data)
     print(emails)
-    fp = open('email', 'w')
-    fp.write(str(emails))
-    ph_no=number_extraction(data)
-
+    with open('email.txt', 'w') as fp:
+        fp.write(str(emails))
+    ph_no = number_extraction(data)
     print(ph_no)
-    fp = open('ph_no', 'w')
-    fp.write(str(ph_no))
-
-    spcl_char=special_char(data)
+    with open('ph_no.txt', 'w') as fp:
+        fp.write(str(ph_no))
+    spcl_char = special_char(data)
     print(spcl_char)
-    fp=open('special_char','w')
-    fp.write(str(spcl_char))
+    with open('special_char.txt', 'w') as fp:
+        fp.write(str(spcl_char))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
